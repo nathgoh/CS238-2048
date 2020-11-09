f42d13322a1cff8deafe843a9b595f5182fc1ff8
Simon Tao <simon@My-Air.local>
2020-11-08 17:42:33 -0800

    cleaned up monte carlo code

--- monte_carlo_agent.py
+++ monte_carlo_agent.py
@@ -20,13 +20,10 @@ def random_run(game):
         move = random.randint(0, 3)
         game_copy.make_move(move)
     
-    return game_copy.max_num(), game_copy.get_sum(), game_copy.get_merge_score()
+    return game_copy.get_sum(), game_copy.max_num(), game_copy.get_merge_score()
 
 def monte_carlo_iter(game):
-    best_move = None
-    best_total_sum = -1
-    best_total_largest = -1
-    best_total_merge = -1
+    total_score = [0, 0, 0, 0]
 
     # For each move (0 - 3)
     for move in range(0,4):
@@ -35,38 +32,25 @@ def monte_carlo_iter(game):
         if str(game_copy) == str(game):
             continue
 
-        total_sum = 0
-        total_largest = 0
-        total_merge_score = 0
-
         # Try lots of paths with that move using random rollout policy
         for i in range(NUM_ITERS):
             output = random_run(game_copy)   # 0 for largest tile, 1 for sum
-            total_largest += output[0] 
-            total_sum += output[1]
-            total_merge_score += output[2]
+            
+            # Eval Method 0: Best total sum
+            if EVAL_METHOD == 0:
+                total_score[move] += output[0]
 
-        # Eval Method 0: Best total sum
-        if EVAL_METHOD == 0:
-            if total_sum > best_total_sum:
-                best_move = move
-                best_total_sum = total_sum
-        
-        # Eval Method 1: Largest tile number, and best total sum
-        elif EVAL_METHOD == 1:
-            if total_largest > best_total_largest or (total_largest == best_total_largest and total_sum > best_total_sum):
-                best_move = move
-                best_total_largest = total_largest
-                best_total_sum = total_sum
-        
-        # Eval Method 2: Largest total merge score
-        elif EVAL_METHOD == 2:
-            if total_merge_score > best_total_merge:
-                best_move = move
-                best_total_merge = total_merge_score
+            # Eval Method 1: Largest tile number
+            elif EVAL_METHOD == 1:
+                total_score[move] += output[1]
 
+            # Eval Method 2: Largest total merge score
+            elif EVAL_METHOD == 2:
+                total_score[move] += output[2]
+
+    best_move = total_score.index(max(total_score))
     game.make_move(best_move)
-    # print(game)
+    print(game)
     # print(game.max_num())
     # print(game.get_sum())
     # print(game.get_merge_score())
@@ -74,8 +58,11 @@ def monte_carlo_iter(game):
 
 def monte_carlo_run():
     game = Game2048()
+    i = 0
     while not game.game_end:
+        print("Iteration: ", i)
         monte_carlo_iter(game)
+        i += 1
 
     print("Max Square Value: {}".format(game.max_num()))
     print("Total Square Sum: {}".format(game.get_sum()))
@@ -105,6 +92,7 @@ def main():
     
     start_time = time.time()
     for i in range(100):
+        print("Repetition Number ", i) 
         max_val_results[i], total_sum_results[i], total_merge_score[i] = monte_carlo_run()
     end_time = time.time()
         
